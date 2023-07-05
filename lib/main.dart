47d2bda9e32f64e8710ad660d3a121d339926272
David Atlan <david@davipro.fr>
2023-07-05 16:07:42 +0200

    Added button on first page to navigate to second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Flutter Login Design',
     home: FirstPage(),
@@ -9,11 +9,32 @@ void main() {
 }
 
 class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Première page'),
+        title: const Text('Première page'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: () {  },
+        child: const Text('Aller à la deuxième page'),
+        ),
+      ),
+    );
+  }
+}
+
+class RaisedButton {
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deuxième page'),
       ),
     );
   }
